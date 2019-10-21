eed800732cfbaf0ad8c9ea89401b7e90d2957fe2
Troy Williams <troy.williams@learningworks.co.nz>
2019-10-22 10:29:59 +1300

    WIP data mappers.

---
+++ classes/local/mapper/registration_data_mapper.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace enrol_arlo\local\mapper;
+
+defined('MOODLE_INTERNAL') || die();
+
+use enrol_arlo\Arlo\AuthAPI\Resource\Registration as RegistrationResource;
+use enrol_arlo\Arlo\AuthAPI\Entity\Registration as RegistrationEntity;
+
+class registration_data_mapper {
+
+    public function from_resource_collection($collection) : array {
+        return [];
+    }
+
+    public function from_resource_object(RegistrationResource $resource) {
+        $registrationentity = new RegistrationEntity();
+        $registrationentity->setUniqueIdentifier($resource->UniqueIdentifier);
+        $registrationentity->setRegistrationID($resource->RegistrationID);
+        $registrationentity->setCreatedDateTime($resource->CreatedDateTime);
+        return $registrationentity;
+    }
+
+}
